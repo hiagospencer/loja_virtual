75e21a048ba7fbb62914023894a1b9172651328f
hiagospencer <hiaguinhospencer>
2025-01-31 22:06:17 -0300

    criando a funcionalidade de adicionar os itens no carrinho

--- carrinho/views.py
+++ carrinho/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Pedido
-from produto.models import ItensPedido
+from produto.models import *
 
 from .utilidades import preco_total
 
@@ -19,8 +19,15 @@ def adicionar_carrinho(request, id_carrinho):
         dados = request.POST.dict()
         tamanho = dados.get('tamanho')
         id_cor = dados.get('cor')
-        print(tamanho)
-        print(id_cor)
-        return redirect('loja')
+        if request.user.is_authenticated:
+            cliente = request.user.cliente
+        else:
+            return redirect('carrinho')
+        pedido,criado = Pedido.objects.get_or_create(cliente=cliente, finalizado=False)
+        item_estoque = ItemEstoque.objects.get(produto__id=id_carrinho, tamanho=tamanho, cor__id=id_cor)
+        item_pedido, criado = ItensPedido.objects.get_or_create(item_estoque=item_estoque, pedido=pedido)
+        item_pedido.quantidade += 1
+        item_pedido.save()
+        return redirect('carrinho')
     else:
         return redirect('loja')
